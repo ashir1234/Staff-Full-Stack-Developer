4aae89fd51bef71cceab4073d1d5146269911d8a
ashir1234 <ashirharoon15@gmail.com>
2024-03-18 13:36:09 -0700

    first commit for staff full stack

---
+++ src/components/About.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <button @click="fetchData">Fetch Data</button>
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+            <h2>Data:</h2>
+            <ul>
+                <li v-for="item in data" :key="item.id">{{ item.title }}</li>
+            </ul>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+    import axios from 'axios';
+    
+    export default {
+        name: 'About',
+        components: {
+        },
+        
+        data(){
+            return {
+                initialState: 'asdasd',
+                loading: false,
+                data: []
+            }
+        },
+        mounted() {
+            this.callConsole()
+        },
+        methods: {
+            callConsole(){
+                console.log(this.initialState)
+            },
+            async fetchData() {
+                this.loading = true;
+                try {
+                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+                    this.data = response.data;
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                } finally {
+                    this.loading = false;
+                }
+            },
+        }
+    }
+  </script>
+  
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <h2>Home Page</h2>
+      <p>Welcome to the home page!</p>
+    </div>
+  </template>
+  
+<script>
+    export default {
+        name: 'Home',
+        components: {
+        }
+    }
+</script>
